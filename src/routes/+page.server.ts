155b8a9f76a8242d059f27a394089133a8eb9fea
Leftium <381217+Leftium@users.noreply.github.com>
2025-07-07 12:06:57 -0500

    Output LoadEvent to page.

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,107 @@
+async function serializeRequest(request: Request) {
+	const {
+		method,
+		cache,
+		credentials,
+		destination,
+		integrity,
+		keepalive,
+		mode,
+		redirect,
+		referrer,
+		referrerPolicy
+		// isReloadNavigation,
+		// isHistoryNavigation
+	} = request;
+
+	// prettier-ignore
+	return {
+		url:     request.url,
+		headers: Object.fromEntries(request.headers.entries()),
+		body:    await request.clone().text(),
+		method,
+		destination,
+		referrer,
+		referrerPolicy,
+		mode,
+		credentials,
+		cache,
+		redirect,
+		integrity,
+		keepalive
+		//isReloadNavigation,
+		//isHistoryNavigation
+	};
+}
+
+export const load = async (loadEvent) => {
+	//console.log(loadEvent);
+
+	const { isDataRequest, isSubRequest, route, locals } = loadEvent;
+
+	// prettier-ignore
+	const loadEventSerialized = {     
+		request:          await serializeRequest(loadEvent.request),
+		url:              loadEvent.url.href,
+		cookies:          loadEvent.cookies.getAll(),
+		getClientAddress: loadEvent.getClientAddress(),
+        platform:         loadEvent.platform || null,
+
+        params:           {...loadEvent.params},
+		route,
+		isDataRequest,
+		isSubRequest,
+        locals,
+	};
+
+	function getType(v: unknown) {
+		if (typeof v !== 'object') {
+			return typeof v;
+		} else if (v === null) {
+			return 'null';
+		}
+		const cname = v.constructor.name;
+		return cname === 'Object' ? 'object' : cname;
+	}
+
+	// Ensure function and undefined sorted last
+	const sortingType = ({ type1, type2 }: { type1: string; type2: string }) =>
+		`${[type1, 'undefined'].includes(type2) ? '' : '_'}${type1}${type2}`
+			.replaceAll('function', 'y')
+			.replaceAll('undefined', 'z');
+
+	const keys = Object.keys(loadEvent)
+		.map((k) => {
+			const key = k as keyof typeof loadEvent;
+			const type1 = getType(loadEvent[key]);
+			const type2 = getType(loadEventSerialized[key as keyof typeof loadEventSerialized]);
+			const status = Object.keys(loadEventSerialized).includes(key)
+				? '*'
+				: type1 === 'function'
+					? ' '
+					: '?';
+
+			return { key, type1, type2, status };
+		})
+		.sort((a, b) => {
+			const aType = sortingType(a);
+			const bType = sortingType(b);
+			if (aType === bType) {
+				return a.key > b.key ? 1 : -1;
+			}
+			return aType > bType ? 1 : -1;
+		});
+
+	console.log(loadEvent);
+	console.log(loadEventSerialized);
+	console.log(
+		keys
+			.map(
+				({ status, key, type1, type2 }) =>
+					`${status} ${key.padEnd(16)} ${type1.padEnd(9)} ${(type1 === type2 ? ' ' : type2).padEnd(10)}`
+			)
+			.join('\n')
+	);
+
+	return { loadEventSerialized, keys };
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	let { data } = $props();
+
+	console.log(data.loadEventSerialized)
+</script>
+
+<h1>ServerLoadEvent keys</h1>
+<pre>{data.keys
+		.map(
+			({ status, key, type1, type2 }) =>
+				`${status} ${key.padEnd(16)} ${type1.padEnd(9)} ${type1 === type2 ? '' : type2}`
+		)
+		.join('\n')}</pre>
+
+<h1>ServerLoadEvent</h1>
+<pre>{JSON.stringify(data.loadEventSerialized, null, 4)}</pre>
